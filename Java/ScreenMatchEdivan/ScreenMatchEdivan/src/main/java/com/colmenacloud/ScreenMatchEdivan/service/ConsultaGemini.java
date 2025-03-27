35fa70a9225b1d34da9fb867213e6a15f43373f0
EdivanL <edivanludvig@gmail.com>
2025-03-27 10:59:12 -0300

    1.7 - Ignore

--- Java/ScreenMatchEdivan/ScreenMatchEdivan/src/main/java/com/colmenacloud/ScreenMatchEdivan/service/ConsultaGemini.java
+++ Java/ScreenMatchEdivan/ScreenMatchEdivan/src/main/java/com/colmenacloud/ScreenMatchEdivan/service/ConsultaGemini.java
@@ -8,7 +8,7 @@ public class ConsultaGemini {
     public static String obterTraducao(String texto) {
 
         ChatLanguageModel gemini = GoogleAiGeminiChatModel.builder()
-                .apiKey("APIKEY")
+                .apiKey("TESTE")
                 .modelName("gemini-1.5-flash")
                 .build();
 
